65c7f637ab67fe633544c629ec36a4e12c1943b6
李洪江 <lihongjiang@supets.com>
2016-09-27 17:19:59 +0800

    修改配置

--- threepartybase/src/main/java/com/supets/pet/threepartybase/api/KeyAndSecrets.java
+++ threepartybase/src/main/java/com/supets/pet/threepartybase/api/KeyAndSecrets.java
@@ -11,20 +11,20 @@ package com.supets.pet.threepartybase.api;
 public interface KeyAndSecrets {
 
     // 微信APP ID
-    String WEIXINAPPID = "wxae4f486adfe33890";
+    String WEIXINAPPID = "XXXXXX";
     String WEIXIN_SCOPE = "snsapi_userinfo";
-    String WEIXIN_APPSECRET = "e05ab5d117ba1274101666d818e8a83f";
-    String WEIXIN_MERCHANT_ID = "1336542101";
+    String WEIXIN_APPSECRET = "XXXXX";
+    String WEIXIN_MERCHANT_ID = "XXXXX";
 
     // sina weibo APP KEY
-    String SINAWEIBO_APPKEY = "2542052748";
+    String SINAWEIBO_APPKEY = "XXXXX";
     String SINAWEIBO_REDIRECT_URI = "https://api.weibo.com/oauth2/default.html";
-    String SINAWEIBO_AppSecret = "3201744bab19c98a69cb663f27ee24fb";
+    String SINAWEIBO_AppSecret = "XXXXXXX";
     String SINAWEIBO_SCOPE = "email,direct_messages_read,direct_messages_write,"
             + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
             + "follow_app_official_microblog," + "invitation_write";
 
     // qq APP ID
-    String QQ_APPID = "1105254618";
+    String QQ_APPID = "XXXXXX";
     String QQ_SCOPE = "all";
 }
