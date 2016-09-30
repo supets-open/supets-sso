8b7950f204f48098ab6ea44e67f42005d270517a
李洪江 <lihongjiang@supets.com>
2016-09-30 11:26:35 +0800

    更新

--- threepartybase/src/main/java/com/supets/pet/threepartybase/api/KeyAndSecrets.java
+++ threepartybase/src/main/java/com/supets/pet/threepartybase/api/KeyAndSecrets.java
@@ -6,19 +6,19 @@ package com.supets.pet.threepartybase.api;
 public interface KeyAndSecrets {
 
     //wechat
-    String WEIXINAPPID = "wxae4f486adfe33890";
+    String WEIXINAPPID = "替换你的应用";
     String WEIXIN_SCOPE = "snsapi_userinfo";
-    String WEIXIN_APPSECRET = "e05ab5d117ba1274101666d818e8a83f";
+    String WEIXIN_APPSECRET = "替换你的应用";
 
     //sina
-    String SINAWEIBO_APPKEY = "2542052748";
+    String SINAWEIBO_APPKEY = "替换你的应用";
     String SINAWEIBO_REDIRECT_URI = "https://api.weibo.com/oauth2/default.html";
-    String SINAWEIBO_AppSecret = "3201744bab19c98a69cb663f27ee24fb";
+    String SINAWEIBO_AppSecret = "替换你的应用";
     String SINAWEIBO_SCOPE = "email,direct_messages_read,direct_messages_write,"
             + "friendships_groups_read,friendships_groups_write,statuses_to_me_read,"
             + "follow_app_official_microblog," + "invitation_write";
 
     //QQ
-    String QQ_APPID = "1105254618";
+    String QQ_APPID = "替换你的应用";
     String QQ_SCOPE = "all";
 }
